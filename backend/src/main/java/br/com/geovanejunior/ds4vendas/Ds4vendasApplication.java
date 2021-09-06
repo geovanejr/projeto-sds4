134181c94959f5ba1acab63f5cbfd858767a29bc
Geovane Junior <geovane.gjunior@gmail.com>
2021-09-06 16:03:59 -0300

    Project created

---
+++ backend/src/main/java/br/com/geovanejunior/ds4vendas/Ds4vendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.geovanejunior.ds4vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ds4vendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ds4vendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/geovanejunior/ds4vendas/Ds4vendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.geovanejunior.ds4vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ds4vendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
